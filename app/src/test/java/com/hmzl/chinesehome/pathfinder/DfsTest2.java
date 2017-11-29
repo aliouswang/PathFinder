8edf760ae4db03e9123be3ed515ae14be59c48b7
aliouswang <wsw622@126.com>
2017-11-29 11:37:42 +0800

    Def test 2

--- app/src/test/java/com/hmzl/chinesehome/pathfinder/DfsTest2.java
+++ app/src/test/java/com/hmzl/chinesehome/pathfinder/DfsTest2.java
@@ -1,8 +1,43 @@
 package com.hmzl.chinesehome.pathfinder;
 
+import java.util.Arrays;
+
 /**
  * Created by aliouswang on 2017/11/29.
  */
 
 public class DfsTest2 {
+
+    private int n;
+    private int[] a;
+    private int[] book;
+
+    public DfsTest2(int n) {
+        this.n = n;
+        a = new int[n];
+        book = new int[n];
+    }
+
+    public void dfs(int step) {
+        if(step == n) {
+            System.out.println(Arrays.toString(a));
+            return;
+        }
+        for (int i = 0;i < n; i++) {
+            if (book[i] == 0) {
+                book[i] = 1;
+                a[step] = i;
+                dfs(step + 1);
+                book[i] = 0;
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        DfsTest2 dfsTest = new DfsTest2(3);
+        dfsTest.dfs(0);
+    }
+
 }
